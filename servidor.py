acb2b9b374c6d5cb126d5f9ce1fae93ea5e926de
Jesus Díaz Mata <a24dimaje@iesgrancapitan.org>
2026-04-10 15:21:29 +0200

    Add files via upload

--- servidor.py
+++ servidor.py
@@ -7,45 +7,72 @@ CORS(app) # Esto evita el error de "Cross-Origin" en el navegador de tu compañe
 
 # Datos de ejemplo (Mock local) según el formato que acordasteis
 sensores_db = [
-  {
-    "id": "sensor-01",
-    "nombre": "Jardin Botanico",
-    "lat": 37.867682,
-    "lng": -4.786393,
-    "actual": { "nivel": 45, "caudal": None },
-    "historico": [
-      { "lluvia_mm": 5, "nivel_maximo": 52 },
-      { "lluvia_mm": 10, "nivel_maximo": 63 },
-      { "lluvia_mm": 20, "nivel_maximo": 78 },
-      { "lluvia_mm": 35, "nivel_maximo": 91 }
-    ]
-  },
-  {
-    "id": "sensor-02",
-    "nombre": "Avenida de Corregidor",
-    "lat": 37.873105,
-    "lng": -4.785686,
-    "actual": { "nivel": None, "caudal": 210 },
-    "historico": [
-      { "lluvia_mm": 5, "caudal_maximo": 180 },
-      { "lluvia_mm": 10, "caudal_maximo": 280 },
-      { "lluvia_mm": 20, "caudal_maximo": 410 },
-      { "lluvia_mm": 35, "caudal_maximo": 580 }
-    ]
-  },
-  {
-    "id": "sensor-03",
-    "nombre": "Av. Fray Albino",
-    "lat": 37.871733,
-    "lng": -4.780790,
-    "actual": { "nivel": 62, "caudal": 190 },
-    "historico": [
-      { "lluvia_mm": 5, "nivel_maximo": 68, "caudal_maximo": 220 },
-      { "lluvia_mm": 10, "nivel_maximo": 75, "caudal_maximo": 310 },
-      { "lluvia_mm": 20, "nivel_maximo": 88, "caudal_maximo": 450 },
-      { "lluvia_mm": 35, "nivel_maximo": 96, "caudal_maximo": 620 }
-    ]
-  }
+    {
+        "id": "sensor-01",
+        "nombre": "Avenida de Corregidor",
+        "lat": 37.873105,
+        "lng": -4.785686,
+        "actual": { "nivel": 91, "caudal": 410 },
+        "historico": [
+            { "lluvia_mm": 5, "nivel_maximo": 52, "caudal_maximo": 180 },
+            { "lluvia_mm": 20, "nivel_maximo": 85, "caudal_maximo": 390 }
+        ]
+    },
+    {
+        "id": "sensor-02",
+        "nombre": "Jardín Botánico",
+        "lat": 37.867682,
+        "lng": -4.786393,
+        "actual": { "nivel": 78, "caudal": 290 },
+        "historico": [
+            { "lluvia_mm": 5, "nivel_maximo": 40, "caudal_maximo": 150 },
+            { "lluvia_mm": 20, "nivel_maximo": 70, "caudal_maximo": 260 }
+        ]
+    },
+    {
+        "id": "sensor-03",
+        "nombre": "Av. Fray Albino",
+        "lat": 37.871733,
+        "lng": -4.780790,
+        "actual": { "nivel": 45, "caudal": 160 },
+        "historico": [
+            { "lluvia_mm": 10, "nivel_maximo": 30, "caudal_maximo": 100 },
+            { "lluvia_mm": 30, "nivel_maximo": 50, "caudal_maximo": 180 }
+        ]
+    },
+    {
+        "id": "sensor-04",
+        "nombre": "Av. Conde de Vallellano",
+        "lat": 37.876438,
+        "lng": -4.786218,
+        "actual": { "nivel": 62, "caudal": 220 },
+        "historico": [
+            { "lluvia_mm": 5, "nivel_maximo": 45, "caudal_maximo": 160 },
+            { "lluvia_mm": 20, "nivel_maximo": 65, "caudal_maximo": 240 }
+        ]
+    },
+    {
+        "id": "sensor-05",
+        "nombre": "Acera mira al río",
+        "lat": 37.875659,
+        "lng": -4.776240,
+        "actual": { "nivel": 30, "caudal": 95 },
+        "historico": [
+            { "lluvia_mm": 5, "nivel_maximo": 20, "caudal_maximo": 70 },
+            { "lluvia_mm": 20, "nivel_maximo": 35, "caudal_maximo": 110 }
+        ]
+    },
+    {
+        "id": "sensor-06",
+        "nombre": "Calle de Pio XII",
+        "lat": 37.871171,
+        "lng": -4.774394,
+        "actual": { "nivel": 30, "caudal": 95 },
+        "historico": [
+            { "lluvia_mm": 5, "nivel_maximo": 15, "caudal_maximo": 60 },
+            { "lluvia_mm": 20, "nivel_maximo": 32, "caudal_maximo": 100 }
+        ]
+    }
 ]
 
 
